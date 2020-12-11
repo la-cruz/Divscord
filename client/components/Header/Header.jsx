ae2f329168072a6e5e83593ec7cafdf5c8ed3333
Mickael Alves <0mickael.alves0@gmail.com>
2020-12-11 18:29:09 +0100

    Adding style for home page and header

--- client/components/Header/Header.jsx
+++ client/components/Header/Header.jsx
@@ -1,8 +1,56 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import HomeIcon from '@material-ui/icons/Home';
+import { Link } from 'react-router-dom';
+import AppLogo from '../../assets/images/app_logo.png';
+
+const useStyles = makeStyles({
+  headerContainer: {
+    position: 'fixed',
+    width: '100vw',
+    fontFamily: 'Raleway, Arial',
+    display: 'flex',
+    height: '10vh',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  appLogo: {
+    maxWidth: '40px',
+    margin: '0 1rem',
+  },
+  iconBtn: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'white',
+    textTransform: 'uppercase',
+    margin: '0 1rem',
+    padding: '0.5rem 1.5rem',
+    fontWeight: 'bold',
+    borderRadius: '50px',
+    background: 'linear-gradient(to right, #5c258d, #4389a2)',
+    boxShadow: '0px 0px 8px rgba(150, 150, 150, 1)',
+  },
+  icon: {
+    marginRight: '0.5rem',
+    fontSize: '1.5rem',
+    textShadow: '0px 0px 3px rgba(150, 150, 150, 1)',
+  },
+});
 
 function Header() {
+  const classes = useStyles();
+
   return (
-    <h1>TP3 TWS 1</h1>
+    <>
+      <div className={classes.headerContainer}>
+        <img className={classes.appLogo} src={AppLogo} alt="Logo App The Board" />
+        <Link to="/" className={classes.iconBtn}>
+          <HomeIcon className={classes.icon} />
+          Home
+        </Link>
+      </div>
+    </>
   );
 }
 
---
+++ client/components/Home/Home.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import { Link } from 'react-router-dom';
+import VoiceChatIcon from '@material-ui/icons/VoiceChat';
+import ChatIcon from '@material-ui/icons/Chat';
+
+const useStyles = makeStyles({
+  homeContainer: {
+    fontFamily: 'Raleway, Arial',
+    display: 'flex',
+    minHeight: '100vh',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  appLogo: {
+    maxWidth: '200px',
+    display: 'block',
+    margin: '2rem auto',
+  },
+  boardLogo: {
+    height: '50px',
+    paddingRight: '2rem',
+  },
+  paperList: {
+    width: '500px',
+    maxWidth: '100vw',
+  },
+  icon: {
+    fontSize: '4rem',
+    color: 'white',
+    textShadow: '0px 0px 8px rgba(150, 150, 150, 1)',
+  },
+  paper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: '2rem 2rem',
+    margin: '2rem 0',
+    background: 'linear-gradient(to right, #5c258d, #4389a2)',
+  },
+  textList: {
+    color: 'white',
+    textTransform: 'uppercase',
+    fontSize: '1.5rem',
+    fontWeight: 'bold',
+    textShadow: '0px 0px 8px rgba(50, 50, 50, 1)',
+  },
+});
+
+function Home() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.homeContainer}>
+      <Link to="/data-chat" className={classes.paperList}>
+        <Paper elevation={3} className={classes.paper}>
+          <ChatIcon className={classes.icon} />
+          <p className={classes.textList}>Text session</p>
+        </Paper>
+      </Link>
+      <Link to="/video-chat" className={classes.paperList}>
+        <Paper elevation={3} className={classes.paper}>
+          <VoiceChatIcon className={classes.icon} />
+          <p className={classes.textList}>Call session</p>
+        </Paper>
+      </Link>
+    </div>
+  );
+}
+
+export default Home;
